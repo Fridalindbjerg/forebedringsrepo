8eb0048ff20e2ff757599a9858ee8de52d4d6527
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-12-02 15:50:54 +0100

    feat: enhance Blogposts component with comments count and navigation, and add blog post detail page

--- app/(routes)/blog/page.tsx
+++ app/(routes)/blog/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 const Blogposts = async () => {
   "use server";
 
-  const response = await fetch("http://localhost:4000/blogposts?embed=comments");
+  const response = await fetch("http://localhost:4000/blogposts?embed=comments&limit=3");
   const posts = await response.json();
 
   return (
@@ -9,16 +11,26 @@ const Blogposts = async () => {
       <h1 className="text-3xl font-bold mb-6">De 3 nyeste blogposts</h1>
       <div className="">
         {posts.map((post) => (
-          <div key={post.id} className="mb-4  ">
+          <div key={post.id} className=" grid grid-cols-2 ">
             <div className="">
               <img src={post.asset.url} alt={post.title} className="my-2" />
             </div>
-            <div className="">
+            <div className="flex gap-4 flex-col">
               <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{post.author}</p>
-              <p>{post.content}</p>
+              <div className="flex flex-row gap-2 text-(--pink) font-semibold">
+                <p>{post.author}</p>
+                <span>/</span>
+                <p>{post.comments?.length ?? 0} comments</p>
+                <span>/</span>
+                <p>date: NA</p>
+              </div>
+              <p className="line-clamp-6">{post.content}</p>
 
-              <p>{post.comments?.length ?? 0} comments</p>
+              <div className="flex justify-end">
+                <Link href={`/blogposts/${post.id}`} className="pt-4 pb-4 border-t border-white border-b text-sm">
+                  READ MORE
+                </Link>
+              </div>
             </div>
           </div>
         ))}
---
+++ app/(routes)/blogposts/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { h1 } from "framer-motion/client";
+
+const Blog = () => {
+  return <h1>blog post paste</h1>;
+};
+
+export default Blog;
