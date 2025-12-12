243e5a18adfe28df0da5bd90bd37f80cb2a9d726
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-12-12 10:47:16 +0100

    feat: enhance table component with improved layout and image handling

--- app/(routes)/booktable/components/tables.tsx
+++ app/(routes)/booktable/components/tables.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { table } from "console";
+import { isReservedPage } from "next/dist/build/utils";
 import Image from "next/image";
 
 export type Reservation = {
@@ -15,7 +17,13 @@ export type Reservation = {
 // første del er de props vi sender med til Table komponenten. efter : definerer vi typen af hver prop i typescript. hvis der findes en disabled så er det en boolean.
 function Table({ number, image, onPick, disabled = false }: { number: number; image: string; onPick: (n: number) => void; disabled?: boolean }) {
   return (
-    <button type="button" onClick={() => !disabled && onPick(number)} disabled={disabled} className={["relative w-32 h-32 rounded-xl transition", disabled ? "opacity-40 cursor-not-allowed" : "cursor-pointer"].join(" ")} aria-label={`Table ${number}`}>
+    <button
+      type="button"
+      onClick={() => !disabled && onPick(number)}
+      disabled={disabled}
+      className={["relative w-32 h-32 rounded-xl transition", disabled ? "opacity-40 cursor-not-allowed" : "cursor-pointer"].join(" ")}
+      aria-label={`Table ${number}`}
+    >
       <Image src={image} alt={`Table ${number}`} fill className="object-contain" sizes="(max-width:640px) 96px, (max-width:1024px) 128px, 160px" loading="eager" />
       <span className="pointer-events-none absolute inset-0 flex items-center justify-center">
         <span className="text-white font-bold px-2 py-1 ">{number}</span>
@@ -25,11 +33,12 @@ function Table({ number, image, onPick, disabled = false }: { number: number; im
 }
 
 export default function Tables({ onPick, reservedTables }: { onPick: (n: number) => void; reservedTables: Reservation[] }) {
+
   const tableArr = [
     {
       number: 1,
       img: "/assets/table/table_1.png",
-      isReserved: false,
+      isReserved: true,
     },
     {
       number: 2,
@@ -38,7 +47,7 @@ export default function Tables({ onPick, reservedTables }: { onPick: (n: number)
     },
     {
       number: 3,
-      img: "/assets/table/table_1.png",
+      img: "/assets/table/table_2.png",
       isReserved: true,
     },
     {
@@ -48,7 +57,7 @@ export default function Tables({ onPick, reservedTables }: { onPick: (n: number)
     },
     {
       number: 5,
-      img: "/assets/table/table_1.png",
+      img: "/assets/table/table_3.png",
       isReserved: true,
     },
     {
@@ -58,7 +67,7 @@ export default function Tables({ onPick, reservedTables }: { onPick: (n: number)
     },
     {
       number: 7,
-      img: "/assets/table/table_2.png",
+      img: "/assets/table/table_1.png",
       isReserved: true,
     },
     {
@@ -68,45 +77,61 @@ export default function Tables({ onPick, reservedTables }: { onPick: (n: number)
     },
     {
       number: 9,
-      img: "/assets/table/table_2.png",
+      img: "/assets/table/table_1.png",
       isReserved: true,
     },
     {
       number: 10,
+      img: "/assets/table/table_3.png",
+      isReserved: true,
+    },
+    {
+      number: 11,
+      img: "/assets/table/table_1.png",
+      isReserved: true,
+    },
+    {
+      number: 12,
+      img: "/assets/table/table_1.png",
+      isReserved: true,
+    },
+    {
+      number: 13,
       img: "/assets/table/table_2.png",
+      isReserved: true,
+    },
+    {
+      number: 14,
+      img: "/assets/table/table_1.png",
+      isReserved: true,
+    },
+    {
+      number: 15,
+      img: "/assets/table/table_3.png",
       isReserved: true,
     },
   ];
 
   return (
-    <div className="w-full mx-auto p-4">
-      <h1 className="text-center text-3xl font-bold mb-8">BOOK TABLE</h1>
-
-      <div className="grid gap-4 grid-cols[repeat(auto-fill,_minmax(180px,_1fr))]">
-        {tableArr.map((table, id) => {
+    <div className="grid grid-cols-3 md:grid-cols-5 gap-4 place-items-center">
+       {tableArr.map((table, id) => {
           const isReserved = reservedTables.find((res) => {
             return res.tablenumber === table.number;
           });
 
           return <Table key={id} number={table.number} image={table.img} onPick={onPick} disabled={Boolean(isReserved)} />;
         })}
-
-        {/* <Table number={1} image="/assets/table/table_1.png" onPick={onPick} />
-        <Table number={2} image="/assets/table/table_1.png" onPick={onPick} />
-        <Table number={3} image="/assets/table/table_1.png" onPick={onPick} />
-        <Table number={4} image="/assets/table/table_1.png" onPick={onPick} />
-        <Table number={5} image="/assets/table/table_1.png" onPick={onPick} />
-        <Table number={6} image="/assets/table/table_2.png" onPick={onPick} />
-        <Table number={7} image="/assets/table/table_2.png" onPick={onPick} />
-        <Table number={8} image="/assets/table/table_2.png" onPick={onPick} />
-        <Table number={9} image="/assets/table/table_2.png" onPick={onPick} />
-        <Table number={10} image="/assets/table/table_2.png" onPick={onPick} />
-        <Table number={11} image="/assets/table/table_3.png" onPick={onPick} />
-        <Table number={12} image="/assets/table/table_3.png" onPick={onPick} />
-        <Table number={13} image="/assets/table/table_3.png" onPick={onPick} />
-        <Table number={14} image="/assets/table/table_3.png" onPick={onPick} />
-        <Table number={15} image="/assets/table/table_3.png" onPick={onPick} /> */}
-      </div>
     </div>
   );
 }
+
+{
+  /* <div className="grid gap-4 space-between [grid-template-columns:repeat(auto-fill,_minmax(200px,_1fr))]"> */
+}
+
+
+
+
+
+
+
