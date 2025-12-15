83c878b26d15951bc156349e2c46eb5388a91bfc
My <idla0003@stud.kea.dk>
2025-12-15 12:08:49 +0100

    feat: enhance welcome section with dynamic card components and animations

--- app/components_home/Section1_welcome.tsx
+++ app/components_home/Section1_welcome.tsx
@@ -1,10 +1,20 @@
 import Card from "./Section1_welcome_card";
-import thumb1 from "@/public/assets/content-img/thumb1.jpg";
+import Index_h2 from "./Index_h2"
+
 
 export default function Section1_welcome() {
   return (
-    <section className="col-[content-start/content-end]">
-      <Card image="/assets/content-img/thumb1.jpg" title="Night Club" text="The point of using Lorem Ipsum is that it has a more-or-less normal distribution." icon="/assets/icons/club.svg" />
+    <section className="col-[content-start/content-end] ">
+      <Index_h2 text="Welcome to nightclub" />
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+      <Card image="/assets/content-img/thumb1.jpg" title="Night Club" text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable." icon="/assets/icon/favicon.png" />
+
+      <Card image="/assets/content-img/thumb1.jpg" title="Restaurant" text="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution."   icon="/assets/icon/favicon.png"  />
+
+      <Card image="/assets/content-img/thumb1.jpg" title="Bar" text="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin." icon="/assets/icon/favicon.png" />
+      </div>
     </section>
   );
 }
--- app/components_home/Section1_welcome_card.tsx
+++ app/components_home/Section1_welcome_card.tsx
@@ -1,19 +1,113 @@
+"use client";
+
 import Image from "next/image";
+
+import { motion } from "framer-motion";
 
 type Props = {
   image: string;
   title: string;
   text: string;
-  icon?: string;
+  icon: string;
 };
+
+const Time = { duration: 1.5, ease: "easeOut" as const };
+const Fast = { duration: 0.6, ease: "easeOut" as const };
 
 export default function Card({ image, title, text, icon }: Props) {
   return (
-    <div className="relative overflow-hidden  group">
-      <Image src={image} alt={title} width={1200} height={800} className="w-full h-auto object-cover"  />
+    // i denne framer-motion bruger vi tilstandende: rest og hover.
+    // "rest" er når bruger ikke rør card
+    // hover er naturligvis ved hover :-)
 
-      <h3 className="text-white text-2xl font-semibold mb-2">{title}</h3>
-      <p className="text-white/80 max-w-[380px]">{text}</p>
-    </div>
+    <motion.div
+      className="relative group grid"
+      // initial="rest": år komponent først indlæses er state = rest
+      initial="rest"
+      // animate="rest": sikrer at komponent altid er i rest når der ikke interageres med det (kan godt udelades, men er godt at have)
+      animate="rest"
+      //  whileHover="hover": ved hover-state skiftes til tilstand: hover. Logisk. Her "afspilles" alle animerede effekter.
+      whileHover="hover"
+      //  whileTap="hover": gør i princippet samme som "whileHover", men reagerer på tap - altså devices med touch funktion (telefon, ipad osv)
+      whileTap="hover"
+    >
+      {/* billede */}
+      <Image src={image} alt={title} width={1200} height={800} className="row-start-1 col-start-1 w-full h-full object-cover" />
+
+      {/* OVERLAY VED HOVER */}
+
+      {/* "overlay div" - Første div styrer mørkt overlay/baggrund */}
+      <motion.div
+        className="row-start-1 col-start-1 w-full h-full bg-black/90
+                      opacity-0 group-hover:opacity-100"
+        variants={{
+          // kort forklaring: state rest= sort overlay ikke synligt, state hover = sort overlay synligt
+          rest: { opacity: 0 },
+          hover: { opacity: 1 },
+        }}
+        transition={Fast}
+      />
+
+      {/*"content div" - Ekstra div til alt indhold - nødvendigt da denne skal have flex, for at positionere ikon, trekanter, h3 og p*/}
+      <motion.div
+        className="row-start-1 col-start-1 w-full h-full px-6
+                      flex flex-col items-center justify-center text-center
+                      opacity-0  group-hover:opacity-100
+                      transition-all "
+        variants={{
+          rest: { opacity: 0 },
+          hover: { opacity: 1 },
+        }}
+        transition={Time}
+      >
+        {/* Icon */}
+        <div className="mb-6 inline-flex items-center justify-center rounded-md p-4 border-2 border-(--pink)">
+          <Image src={icon} alt="" width={48} height={48} className="w-12 h-12 object-contain" />
+        </div>
+
+        {/* Tekst h3 + p */}
+        <motion.h3
+          className="text-white text-2xl font-semibold mb-2"
+          variants={{
+            rest: { opacity: 0, scale: 0.9 },
+            hover: { opacity: 1, scale: 1 },
+          }}
+          transition={Time}
+        >
+          {title}
+        </motion.h3>
+
+        <motion.p
+          className="text-white"
+          variants={{
+            rest: { opacity: 0, x: 40 },
+            hover: { opacity: 1, x: 0 },
+          }}
+          transition={Time}
+        >
+          {text}
+        </motion.p>
+      </motion.div>
+
+      {/* trekanter - vises også kun ved hover */}
+      <motion.div
+        className="absolute top-0 left-0 w-0 h-0 border-r-70 border-r-transparent border-t-70 border-t-(--pink)
+                      opacity-0 group-hover:opacity-100 "
+        variants={{
+          rest: { opacity: 0, y: -40 },
+          hover: { opacity: 1, y: 0 },
+        }}
+        transition={Fast}
+      />
+      <motion.div
+        className="absolute bottom-0 right-0 w-0 h-0 border-l-70 border-l-transparent border-b-70 border-b-(--pink)
+                      opacity-0 group-hover:opacity-100 "
+        variants={{
+          rest: { opacity: 0, y: 40 },
+          hover: { opacity: 1, y: 0 },
+        }}
+        transition={Fast}
+      />
+    </motion.div>
   );
 }
